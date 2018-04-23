536eb69aa4bbd0c88b13802c5322f4ef71c41063
yuanbaoyu <yuanbaoyu@wangchengiMac.local>
2018-04-23 15:22:43 +0800

    添加FloatingActionButton的扩展类，可选中FloatingActionButton

--- app/src/main/java/com/xinghai/widget/MainActivity.java
+++ app/src/main/java/com/xinghai/widget/MainActivity.java
@@ -1,9 +1,6 @@
 package com.xinghai.widget;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,8 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.xinghai.widget.checkableview.CheckableFloatingActionButton;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -25,12 +25,12 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final CheckableFloatingActionButton fab = findViewById(R.id.add_schedule_button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                boolean checked = fab.isChecked();
+                fab.setChecked(!checked);
             }
         });
 
